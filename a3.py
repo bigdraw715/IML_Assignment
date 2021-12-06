f74390a114e750961482ade831b4f99f8cd9cfc5
Dazhuo Qiu <31387201+bigdraw715@users.noreply.github.com>
2021-12-06 11:32:51 +0100

    Update a3.py

--- a3.py
+++ a3.py
@@ -14,19 +14,3 @@ print("Cross Validation Train MSE:",np.mean(scores['train_neg_mean_squared_error
 print("Cross Validation Test MSE:",np.mean(scores['test_neg_mean_squared_error']))
 print("Cross Validation Train R2:",np.mean(scores['train_r2']))
 print("Cross Validation Test R2:",np.mean(scores['test_r2']))
-
-
-
-
-
-
-# rig.fit(X_train2, y_train2)
-# reg = LinearRegression()
-# reg.fit(X_train2, y_train2)
-# y2_predict = rig.predict(X_test2)
-# #print(reg.regressor_.coef_)
-# y_predict = reg.predict(X_test2)
-# print(mean_squared_error(y_predict, y_test2))
-# print(r_square(y_predict, y_test2))
-# print(mean_squared_error(y2_predict, y_test2))
-# print(r_square(y2_predict, y_test2))
