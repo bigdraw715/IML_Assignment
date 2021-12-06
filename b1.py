27d40e0dc5525526d07cf40b77be779b0444d77d
Dazhuo Qiu <31387201+bigdraw715@users.noreply.github.com>
2021-12-06 11:33:46 +0100

    Update b1.py

--- b1.py
+++ b1.py
@@ -1,69 +1,41 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-import pandas as pd
-from preprocess import data_preprocess
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.multiclass import OneVsOneClassifier
-from sklearn.linear_model import LogisticRegression
-import time
+import pandas as pd # Load and manipulate data and for One-Hot Encoding.
+import numpy as np # Data manipulation.
+from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error, r2_score
+from preprocess import *
 
-X_onehot,y_onehot = data_preprocess(method="onehot")
-X_class,y_class = data_preprocess(method="class")
-print("Use class encoded data")
-time.sleep(2)
-print("One vs One calssifier:")
-X = np.array(X_class)
-y = np.array(y_class["category"]).astype(int)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, shuffle=True, random_state=0)
-clf = OneVsOneClassifier(
-    LogisticRegression(max_iter = 10000)).fit(X_train, y_train)
-# clf = LogisticRegression(max_iter = 10000).fit(X_train, y_train)
-print("Train Accuracy:",clf.score(X_train,y_train))
-print("Test Accuracy:",clf.score(X_test,y_test))
+X, y_lst = data_preprocess()
+y = y_lst['Subscribed']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, shuffle=True, random_state=0)
 
-print("One vs All calssifier:")
-X = np.array(X_class)
-y = np.array(y_class["category"]).astype(int)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, shuffle=True, random_state=0)
-clf = OneVsRestClassifier(
-    LogisticRegression(max_iter = 10000)).fit(X_train, y_train)
-# clf = LogisticRegression(max_iter = 10000).fit(X_train, y_train)
-print("Train Accuracy:",clf.score(X_train,y_train))
-print("Test Accuracy:",clf.score(X_test,y_test))
+def SVM(X_train, X_test, y_train, y_test, kernel):
 
-print("\n Use onehot encoded data")
-time.sleep(2)
-print("One vs One calssifier:")
-X = np.array(X_onehot)
-y = np.array(y_onehot["category"]).astype(int)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, shuffle=True, random_state=0)
-clf = OneVsOneClassifier(
-    LogisticRegression(max_iter = 10000)).fit(X_train, y_train)
-# clf = LogisticRegression(max_iter = 10000).fit(X_train, y_train)
-print("Train Accuracy:",clf.score(X_train,y_train))
-print("Test Accuracy:",clf.score(X_test,y_test))
+    mse_lst = []
+    r2_lst = []
 
-print("One vs All calssifier:")
-X = np.array(X_onehot)
-y = np.array(y_onehot["category"]).astype(int)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, shuffle=True, random_state=0)
-clf = OneVsRestClassifier(
-    LogisticRegression(max_iter = 10000)).fit(X_train, y_train)
-# clf = LogisticRegression(max_iter = 10000).fit(X_train, y_train)
-print("Train Accuracy:",clf.score(X_train,y_train))
-print("Test Accuracy:",clf.score(X_test,y_test))
+    model = cross_validate(SVR(C = 0.5, kernel=kernel), X_train, y_train, n_jobs=-1, return_estimator = True)
+    for m in model['estimator']:
+        y_pred = m.predict(X_test)
+        mse_lst.append(mean_squared_error(y_pred, y_test))
+        r2_lst.append(m.score(X_test, y_test))
+
+    mse = np.mean(mse_lst)
+    r2 = np.mean(r2_lst)
+
+    print(kernel+'_mse:', mse, kernel+'_r2:', r2)
+
+SVM(X_train, X_test, y_train, y_test, 'rbf')
+SVM(X_train, X_test, y_train, y_test, 'linear')
+SVM(X_train, X_test, y_train, y_test, 'poly')
 print(
 '''
-                            #####        #   
-#####   ##    ####  #    # #     #      ##   
-  #    #  #  #      #   #        #     # #   
-  #   #    #  ####  ####    #####        #   
-  #   ######      # #  #   #       ###   #   
-  #   #    # #    # #   #  #       ###   #   
-  #   #    #  ####  #    # ####### ### ##### 
+                            #####      #       
+#####   ##    ####  #    # #     #     #    #  
+  #    #  #  #      #   #        #     #    #  
+  #   #    #  ####  ####    #####      #    #  
+  #   ######      # #  #   #       ### ####### 
+  #   #    # #    # #   #  #       ###      #  
+  #   #    #  ####  #    # ####### ###      #  
 '''
-)
+    )
